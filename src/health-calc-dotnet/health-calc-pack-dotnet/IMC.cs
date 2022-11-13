e22bdef180072d69e845afec54a01d4a613e3c7f
celio <celio@rjconsultores.com.br>
2022-11-13 19:05:32 -0300

    Adicionado CALCULO DE IMC

---
+++ src/health-calc-dotnet/health-calc-pack-dotnet/IMC.cs
@@ -0,0 +1,46 @@
+﻿using health_calc_pack_dotnet.Interfaces;
+
+namespace health_calc_pack_dotnet
+{
+    public class IMC :  InterfaceIMC
+    {
+        public double Calc(double Height, double Weight)
+        {
+            if (!IsValidData(Height, Weight))
+                throw new Exception("Parametros invalidos.");
+
+            return Math.Round(Weight * 10000 / (Math.Pow(Height, 2)), 2);
+        }
+
+        public string GetIMCClass(double IMC)
+        {
+            var Result = string.Empty;
+            if (IMC < 18.5)
+                Result = "Abaixo do Peso";
+            else if (IMC >= 18.5  && IMC < 25)
+                Result = "SAUDAVEL";
+            else if (IMC >= 25  && IMC < 30)
+                Result = "Pre-Obeso";
+            else if (IMC >= 30  && IMC < 35)
+                Result = "Obesidade nivel 1";
+            else if (IMC >= 35  && IMC < 40)
+                Result = "Obesidade nivel 2";
+            else if (IMC >= 40 )
+                Result = "Obesidade nivel 3";
+
+            return Result;
+        }
+
+        public bool IsValidData(double Height, double Weigth)
+        {
+            if (Height <= 0 || Weigth <= 0)
+                return false;
+
+            return true;
+        }
+    }
+
+    public class InterfaceIMC
+    {
+    }
+}
---
+++ src/health-calc-dotnet/health-calc-pack-dotnet/Interfaces/InterfaceIMC.cs
@@ -0,0 +1,13 @@
+﻿namespace health_calc_pack_dotnet.Interfaces
+{
+    public interface IMC
+    {
+        double Calc(double Height, double Weigth);
+
+        string GetBMIClass(double IMC);
+
+        bool IsValidData(double Height, double Weigth);
+
+
+    }
+}
